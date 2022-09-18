f90c531a94684d72c4427619978d71bf7c017856
zachemnevseto <domnichanna98@gmail.com>
2022-09-18 18:20:46 +0300

    add-first-test

---
+++ src/test/java/tests/qa/NewTest.java
@@ -0,0 +1,14 @@
+package tests.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NewTest {
+
+    @Test
+    void newTest (){
+        Assertions.assertTrue(4>2);
+
+    }
+
+}
